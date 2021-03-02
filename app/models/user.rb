bd06c04f8db3d58be366b9841ff1b8c1bb2dee6f
Mira <matsakovamira156@gmail.com>
2021-03-02 23:08:32 +0200

    add dob for user and timeslot

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable, :validatable, :omniauthable, :omniauth_providers => [:google_oauth2]
-  has_many :events
-  has_many :experts, :through => :events
-  has_one :vaccine
-  has_one :expert
-  has_one :client
+  has_many :events, dependent: :destroy
+  has_many :experts, :through => :events, dependent: :destroy
+  has_one :vaccine, dependent: :destroy
+  has_one :expert, dependent: :destroy
+  has_one :client, dependent: :destroy
 
   def self.from_omniauth(access_token)
     data = access_token.info
+    binding.pry
     user = User.where(:email => data["email"]).first
 
     unless user
       user = User.create(
           name: data["name"],
           email: data["email"],
+          birthday: Time.now.strftime("%d of %B, %Y"),
           encrypted_password: Devise.friendly_token[0,20]
       )
     end
