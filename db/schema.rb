882dae44f71caacf4622d6bf131b6d9bb862a7e5
Mira <matsakovamira156@gmail.com>
2021-03-08 19:18:06 +0200

    Add default location on the map if address does not exist

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_03_05_192807) do
+ActiveRecord::Schema.define(version: 2021_03_07_105859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  
+
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
