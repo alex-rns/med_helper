a0cbd7d1a9759baf8fe45654b5c9ca101283ad05
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2021-03-20 22:29:06 +0200

    Check fontawesome setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 // import '../stylesheets/application'
+import "@fortawesome/fontawesome-free/js/all";
+import "@fortawesome/fontawesome-free/css/all";
 import "channels"
 import "bootstrap"
 import "jquery-ui/ui/widgets/autocomplete"
