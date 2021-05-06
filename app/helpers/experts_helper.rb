0016250b71b40f2f6efc42d8a475144c24864644
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2021-05-06 22:25:56 +0300

    Test rollback experts helper

--- app/helpers/experts_helper.rb
+++ app/helpers/experts_helper.rb
@@ -6,19 +6,21 @@ module ExpertsHelper
   end
 
   def selector_element(arr)
-    arr.each_with_index.map { |x, i| [x.name, i + 1] }
+    abc = arr.each_with_index.map { |x,i| [x.name, i+1] }
+    abc
   end
 
   def set_time
-    (0..23).map { |x| [x.to_s + ":" + "00", "#{x}" + ":" + "00"] }
+    time = (0..23).map {|x| [x.to_s+":"+"00", "#{x}"+":"+"00"]}
+    time
   end
 
   def check_params(expert, time)
-    x = Time.now.strftime('%A').downcase
-    start_time = expert.send("hw_start_#{x}").to_i
-    end_time = expert.send("hw_end_#{x}").to_i
-    event_time = check_event(expert)
-    time >= Time.now && (start_time..end_time).include?(time.hour) && event_time.exclude?(time.hour)
+     x = Time.now.strftime('%A').downcase
+     start_time = expert.send("hw_start_#{x}").to_i
+     end_time =  expert.send("hw_end_#{x}").to_i
+     event_time = check_event(expert)
+     time >= Time.now&&(start_time..end_time).include?(time.hour)&&event_time.exclude?(time.hour)
   end
 
   def check_event(expert)
