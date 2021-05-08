7a68f937e4d14e2d9903922eea5a8a26c8d3a468
Mira <matsakovamira156@gmail.com>
2021-05-08 09:44:42 +0200

    remove time slots from experts page

--- app/helpers/experts_helper.rb
+++ app/helpers/experts_helper.rb
@@ -13,18 +13,4 @@ module ExpertsHelper
   def set_time
     (0..23).map { |x| %W[#{x}:00 #{x}:00] }
   end
-
-  def check_params(expert, time)
-    x = Time.now.strftime('%A').downcase
-    start_time = expert.send("hw_start_#{x}").to_i
-    end_time = expert.send("hw_end_#{x}").to_i
-    event_time = check_event(expert)
-    time >= Time.now && (start_time..end_time).include?(time.hour) && event_time.exclude?(time.hour)
-  end
-
-  def check_event(expert)
-    arr = []
-    expert.events.each { |ev| arr << (ev.start_time.hour..ev.end_time.hour).to_a }
-    arr.flatten
-  end
 end
